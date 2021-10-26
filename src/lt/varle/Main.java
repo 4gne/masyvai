744428eb69b19491c9cde25dd9474dc51d63cfee
4gne <[kiznyte4gne@gmail.com]>
2021-10-26 20:29:53 +0300

    Initial commit

---
+++ src/lt/varle/Main.java
@@ -0,0 +1,50 @@
+package lt.varle;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+// 1.	Sugeneruokite masyvą iš 30 elementų (indeksai nuo 0 iki 29), kurių reikšmės yra atsitiktiniai skaičiai
+//	    nuo 5 iki 25. Atsispausdinate, pasižiūrite ką gavote.
+
+  /*         int arr [] = new int[30];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = (int) (Math.random() * 16) + 5;
+                System.out.print(+arr[i]+ " ");    // printing each array element
+            }   */
+
+
+        // 2.   Naudodamiesi 1 uždavinio masyvu:
+        //        a) Suskaičiuokite kiek masyve yra reikšmių didesnių už 10;
+
+
+        int arr [] = new int[30];
+        int count = 0;
+        if ( arr [i] > 10, count++)
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 16) + 5;
+            System.out.print(+arr[i]+ " ");    
+        }
+
+/*        int arr [] = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            for ( int count = 0; arr[i] > 10, count++) ;
+            arr[i] = (int) (Math.random() * 16) + 5;
+            System.out.print(+arr[i] + " ");
+        }
+            System.out.print(count);
+            }
+ */
+
+        }
+
+        /*  int[] arr = {5, 3, 10};
+        Arrays.sort(arr);
+        for (int i = 0; i< arr.length; i++) {
+            System.out.println(arr[i]);
+        } */
+
+
+    }
+
